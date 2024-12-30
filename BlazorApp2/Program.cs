e8ece3e1a12032f17bdbc219c8ba1e1c9690bfa2
Lukas Rumpala <lukas.rumpala@atlascopco.com>
2024-12-30 16:32:39 +0100

    another try for login/logout

--- BlazorApp2/Program.cs
+++ BlazorApp2/Program.cs
@@ -15,8 +15,34 @@ namespace BlazorApp2
 
             builder.Services.AddOidcAuthentication(options =>
             {
+
                 builder.Configuration.Bind("Auth0", options.ProviderOptions);
                 options.ProviderOptions.ResponseType = "code";
+
+            //options.Events = new OpenIdConnectEvents
+            //{
+            //    // handle the logout redirection
+            //    OnRedirectToIdentityProviderForSignOut = (context) =>
+            //    {
+            //        var logoutUri = $"https://{Configuration["Auth0:Domain"]}/v2/logout?client_id={Configuration["Auth0:ClientId"]}";
+
+            //        var postLogoutUri = context.Properties.RedirectUri;
+            //        if (!string.IsNullOrEmpty(postLogoutUri))
+            //        {
+            //            if (postLogoutUri.StartsWith("/"))
+            //            {
+            //                // transform to absolute
+            //                var request = context.Request;
+            //                postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+            //            }
+            //            logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
+            //        }
+
+            //        context.Response.Redirect(logoutUri);
+            //        context.HandleResponse();
+
+            //        return Task.CompletedTask;
+            //    }
             });
 
             await builder.Build().RunAsync();
